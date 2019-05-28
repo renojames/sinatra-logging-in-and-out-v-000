7bbfb4168be43a51705a80a12377641d4892cd70
Reno Sorensen <renojames19@gmail.com>
2019-05-28 04:16:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,8 +25,6 @@ class ApplicationController < Sinatra::Base
     @is_logged_in = Helpers.is_logged_in?(session)
     if Helpers.is_logged_in?(session)
       erb :account
-    elsif
-      erb :error
     end
   end
 
